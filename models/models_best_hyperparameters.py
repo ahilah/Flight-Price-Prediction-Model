35ecbfa75d1611e9eafce413da03eb7772d9b559
a_hilan <anastasiia.hileta.kn.2021@lpnu.ua>
2024-05-23 16:53:42 +0300

    file with best hyperparams for rf, dtree and xgb is done

---
+++ models/models_best_hyperparameters.py
@@ -0,0 +1,3 @@
+params_rf = {'max_depth': None, 'min_samples_leaf': 2, 'min_samples_split': 5, 'n_estimators': 200}
+params_dtree = {'max_depth': None, 'min_samples_leaf': 4, 'min_samples_split': 10}
+params_xgb = {'learning_rate': 0.1, 'max_depth': 10, 'n_estimators': 500}
