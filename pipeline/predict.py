7a8da39ee48c6ce720926507961a73b868abfb5e
a_hilan <anastasiia.hileta.kn.2021@lpnu.ua>
2024-05-23 16:52:58 +0300

    predict script is done

--- pipeline/predict.py
+++ pipeline/predict.py
@@ -13,7 +13,7 @@ pd.set_option('display.max_rows', 500)
 # file paths
 data_path = "D:/programming/information-technologies-of-smart-systems/term-paper/data/final/new_data.csv"
 param_dict_path = "D:/programming/information-technologies-of-smart-systems/term-paper/src/param_dict.pickle"
-model_path = "D:/programming/information-technologies-of-smart-systems/term-paper/models/finalized_model.sav"
+model_path = "D:/programming/information-technologies-of-smart-systems/term-paper/models/finalized_rf_model.sav"
 prediction_result_path = "D:/programming/information-technologies-of-smart-systems/term-paper/data/prediction_results.csv"
 
 # load dataset
@@ -28,18 +28,22 @@ except FileNotFoundError:
 try:
     with open(param_dict_path, 'rb') as f:
         param_dict = pickle.load(f)
+        print("\nParam dict is loaded!")
 except FileNotFoundError:
     print("Error: Parameter dictionary file not found!")
     exit()
 
 # dropping the useless column 'Unnamed: 0'
 ds = ds.drop('Unnamed: 0', axis=1)
+print("column 'Unnamed: 0' is dropped")
 
 # dropping the useless column 'flight'
 ds = ds.drop('flight', axis=1)
+print("column 'flight' is dropped")
 
 # rename 'class' name to 'flight_class', because *class* is python reserved name
 ds.rename(columns={'class': 'flight_class'}, inplace=True)
+print("column 'class' is renamed to 'flight_class'")
 
 # categorical encoding
 for column in columns.cat_columns[0:]:
@@ -59,6 +63,8 @@ X = pd.DataFrame(scaler, columns=X_tmp.columns)
 for column in columns_to_replace:
     ds[column] = X[column]
 
+print("data is normalized")
+
 # define target and features columns
 X = ds[columns.X_columns]
 
@@ -66,6 +72,7 @@ X = ds[columns.X_columns]
 try:
     with open(model_path, 'rb') as f:
         model = pickle.load(f)
+        print("Prediction model is loaded!")
 except FileNotFoundError:
     print("Error: Model file not found!")
     exit()
@@ -74,5 +81,6 @@ try:
     y_pred = model.predict(X)
     ds['price_pred'] = model.predict(X)
     ds.to_csv(prediction_result_path, index=False)
+    print("Predicted prices are in file", prediction_result_path)
 except Exception as e:
     print("Error occurred while predicting:", e)
