f688ae4c46eae36f68d4e9f163a51bcade95b02a
a_hilan <anastasiia.hileta.kn.2021@lpnu.ua>
2024-05-23 11:51:46 +0300

    file for feature encoding and target is ready

--- pipeline/columns.py
+++ pipeline/columns.py
@@ -1,4 +1,4 @@
-cat_columns = ['airline', 'source_city', 'destination_city', 'departure_time', 'arrival_time', 'stops', 'flight_class']       # categorical columns for encoding
+cat_columns = ['airline', 'source_city', 'destination_city', 'departure_time', 'arrival_time', 'stops', 'flight_class']  # categorical columns for encoding
 
 y_column = ['price']      # target variable
-X_columns = ['airline',	'source_city',	'departure_time	stops',	'arrival_time',	'destination_city',	'flight_class',	'duration',	'days_left']
+X_columns = ['airline',	'source_city', 'departure_time', 'stops', 'arrival_time', 'destination_city', 'flight_class', 'duration', 'days_left']
