d091f6d9999303b39b0104c7cc4b9a320795613e
a_hilan <anastasiia.hileta.kn.2021@lpnu.ua>
2024-05-23 11:54:19 +0300

    predict script is ready. refactoring is needed

--- pipeline/predict.py
+++ pipeline/predict.py
@@ -9,10 +9,10 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_rows', 500)
 
 # file paths
-data_path = "D:/programming/information-technologies-of-smart-systems/calculation-and-graphic work/personal-medical-insurance-cost-prediction/data/final/new_data.csv"
-param_dict_path = "D:/programming/information-technologies-of-smart-systems/calculation-and-graphic work/personal-medical-insurance-cost-prediction/src/param_dict.pickle"
-model_path = "D://programming//information-technologies-of-smart-systems//calculation-and-graphic work//personal-medical-insurance-cost-prediction//models//finalized_model.sav"
-prediction_result_path = "D:/programming/information-technologies-of-smart-systems/calculation-and-graphic work/personal-medical-insurance-cost-prediction/data/prediction_results.csv"
+data_path = "D:/programming/information-technologies-of-smart-systems/term-paper/data/final/new_data.csv"
+param_dict_path = "D:/programming/information-technologies-of-smart-systems/term-paper/src/param_dict.pickle"
+model_path = "D:/programming/information-technologies-of-smart-systems/term-paper/models/finalized_model.sav"
+prediction_result_path = "D:/programming/information-technologies-of-smart-systems/term-paper/data/prediction_results.csv"
 
 # load dataset
 try:
@@ -29,6 +29,15 @@ try:
 except FileNotFoundError:
     print("Error: Parameter dictionary file not found!")
     exit()
+
+# dropping the useless column 'Unnamed: 0'
+ds = ds.drop('Unnamed: 0', axis=1)
+
+# dropping the useless column 'flight'
+ds = ds.drop('fight', axis=1)
+
+# rename 'class' name to 'flight_class', because *class* is python reserved name
+ds.rename(columns={'class': 'flight_class'}, inplace=True)
 
 # categorical encoding
 for column in columns.cat_columns[0:]:
@@ -47,7 +56,7 @@ except FileNotFoundError:
 
 try:
     y_pred = model.predict(X)
-    ds['charges_pred'] = model.predict(X)
+    ds['price_pred'] = model.predict(X)
     ds.to_csv(prediction_result_path, index=False)
 except Exception as e:
     print("Error occurred while predicting:", e)
